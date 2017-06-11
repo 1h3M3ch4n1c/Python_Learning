3e7dc43b9d3a9473d67ebc970edface3107ca6d9
Naga Vijayendra <vijay46vr@gmail.com>
2017-06-12 00:42:53 +0530

    Command Line Program One
    Command Line Arguments Programs

---
+++ CommandLineOne.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#Command line program to count the number of
+#Line in the File
+
+#import sys module
+import sys
+
+def main():
+    #Enter the command
+    CommandLine = sys.argv[1]
+    if CommandLine == "--help":
+        print("This is a simple command line program")
+    elif CommandLine == "--length":
+        fileToRead = open("CommandLineOne.py","r")
+        numberOfWords = len(fileToRead.read())
+        fileToRead.close()
+        print("Length of the file: {}".format(numberOfWords))
+    elif CommandLine == "--showcontents":
+        fileToRead = open("CommandLineOne.py","r")
+        fileContent = fileToRead.read()
+        print("File contents: \n{}".format(fileContent))
+        fileToRead.close()
+    else:
+        print("Unrecognised command: {}".format(CommandLine))
+
+if __name__ == "__main__":
+    main()
