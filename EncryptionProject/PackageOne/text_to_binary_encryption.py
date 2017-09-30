ec998b3b83d7b726353519e98cf6f25444c6f3b7
drag0n-9 <drag0n9@localhost.localdomain>
2017-10-01 01:28:02 +0530

    updated the text_to_binary_encryption.py file

--- EncryptionProject/PackageOne/text_to_binary_encryption.py
+++ EncryptionProject/PackageOne/text_to_binary_encryption.py
@@ -1,18 +1,19 @@
 #this program converts text into binary
-text = input("Enter the text:\n")
-binary_number = ""
+text = input("Enter the text:\n")   #input from the user
+binary_number = ""  #variable to store the converted binary number
 
 def binary_conversion(input_text):
     #binary number conversion function
     input_text_number = ord(input_text)
-
     binary_number = ""
 
+    #code to convert to binary number
     while(input_text_number>0):
         binary_number = str(input_text_number%2)+binary_number
         input_text_number = int(input_text_number/2)
     return binary_number
 
+#function calling
 binary_equivalent_text = ""
 for each_letter in text:
     binary_equivalent_text = binary_conversion(each_letter)+binary_equivalent_text
